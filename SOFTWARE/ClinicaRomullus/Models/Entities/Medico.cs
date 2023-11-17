97e49b77a33c891110436443313ebe059252f95a
Romulo Cesar Silvestre Leite <romulo.leite@edu.senac.br>
2023-11-17 19:55:37 -0300

    backup

---
+++ SOFTWARE/ClinicaRomullus/Models/Entities/Medico.cs
@@ -0,0 +1,14 @@
+public class Medico{ //inicio
+    
+    /*
+      variável: é um espaço reservado na memória do 
+      computador.
+    */
+    //POO é programação orientada a objetos
+    //Na POO a variável é chamada também de atributo
+    //No C# ela pode ser um propriedade automática
+    int Id;
+    string? CRM;
+    string Nome = string.Empty;
+
+}//fim
