dfa02bcd1451a32c3dcb3949cdb93cee0430876b
Romulo Cesar Silvestre Leite <romulo.leite@edu.senac.br>
2023-11-17 22:00:22 -0300

    projeto base

--- SOFTWARE/ClinicaRomullus/Models/Contexts/ClinicaRomullusContext.cs
+++ SOFTWARE/ClinicaRomullus/Models/Contexts/ClinicaRomullusContext.cs
@@ -1,10 +1,23 @@
+using ClinicaRomullus.Models.Entities;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.SqlServer;
-
 namespace ClinicaRomullus.Models.Contexts
 {
     public class ClinicaRomullusContext:DbContext
-    {
-        
-    }
+    { 
+        private readonly IConfiguration _configuration;
+        public ClinicaRomullusContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public DbSet<Medico> Medicos => Set<Medico>();        
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("ClinicaRomullusHome"));
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+         
+        }
+    }        
 }
--- SOFTWARE/ClinicaRomullus/Program.cs
+++ SOFTWARE/ClinicaRomullus/Program.cs
@@ -1,8 +1,10 @@
+using ClinicaRomullus.Models.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<ClinicaRomullusContext>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
